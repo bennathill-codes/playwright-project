acc10839988296e105474d7867cfac7021582f9d
Benjamin Hill <bennathill@benjamins-mbp.mynetworksettings.com>
2024-10-30 14:21:56 -0700

    Add POMs as fixtures in test file, refactor tests using helper methods for better readability

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -1,177 +1,137 @@
-import { test, expect, type Page } from '@playwright/test';
+
+import { test, expect } from './base';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
   });
 
-test.describe('Landing Page', () => {
-    test('should log in and log out', async ({ page }) => {
-        await loginStandardUser(page);
-        
-        await expect(page.getByTestId('title')).toHaveText('Products');
-        await page.locator('.bm-burger-button').click();
-        await page.getByTestId('logout-sidebar-link').click();
-        await expect(page.locator('.login_logo')).toHaveText('Swag Labs');
-    });
-
-    test('should not log in with invalid credentials', async ({ page }) => {
-        await loginInvalidUser(page);
+test.describe('Log In Page', () => {
+    test('should not log in with invalid credentials', async ({ loginPage }) => {
+        await loginPage.loginInvalidUser();
 
         // assert error banners
-        await expect(page.getByTestId('error')).toBeVisible();
-        await expect(page.getByTestId('error')).toContainText('Username and password do not match');
+        await expect(loginPage.loginError).toBeVisible();
+        await expect(loginPage.loginError).toContainText('Username and password do not match');
+    });
+
+    test('should log in and log out', async ({ loginPage, productPage }) => {
+        await loginPage.loginStandardUser();
+        await expect(productPage.title).toHaveText('Products');
+        
+        await productPage.clickSideBarMenu();
+        await productPage.clickLogOut()
+        await expect(productPage.loginLogo).toBeVisible();
     });
 });
 
-test.describe('Product Page', () => {
-    test('should have correct product count displayed', async ({ page }) => {
-        await loginStandardUser(page);
 
-        await expect(page.getByTestId('inventory-item')).toHaveCount(6);
+test.describe('Product Page', () => {
+    test('should have correct product count displayed', async ({ loginPage, productPage }) => {
+        await loginPage.loginStandardUser();
+
+        await expect(productPage.inventoryItem).toHaveCount(6);
     });
 
-    test('should add and remove items from cart', async ({ page }) => {
-        await loginStandardUser(page);
+    test('should add and remove items from cart', async ({ loginPage, productPage }) => {
+        await loginPage.loginStandardUser();
 
         // add items to cart
-        await addProductToCart(page, 0)
-        await expect(page.getByTestId('remove-sauce-labs-backpack')).toBeVisible();
-
-        await addProductToCart(page, 4)
-        await expect(page.getByTestId('remove-sauce-labs-onesie')).toBeVisible();
+        await productPage.addProductToCart(0)
+        await productPage.addProductToCart(4);
 
         // assert cart item count
-        await expect(page.getByTestId('shopping-cart-badge')).toHaveText('2');
+        await expect(productPage.shoppingCartBadge).toHaveText('2');
 
         // remove items from cart
-        await removeProductFromCart(page, 0);
-        await expect(page.getByTestId('add-to-cart-sauce-labs-backpack')).toBeVisible();
-
-        await removeProductFromCart(page, 4)
-        await expect(page.getByTestId('add-to-cart-sauce-labs-onesie')).toBeVisible();
+        await productPage.removeProductFromCart(0);
+        await productPage.removeProductFromCart(4);
 
         // assert cart item count
-        await expect(page.getByTestId('shopping-cart-badge')).toBeHidden();
+        await expect(productPage.shoppingCartBadge).toBeHidden();
     });
 
-    test('should sort products', async ({ page }) => {
-        await loginStandardUser(page);
-
-        const sortProducts = page.getByTestId('product-sort-container');
+    test('should sort products', async ({ loginPage, productPage }) => {
+        await loginPage.loginStandardUser();
 
         // sort descending
-        await sortProducts.click();
-        await sortProducts.selectOption({ value: 'za' });
-        await validateProductName(page, 0, 'Test.allTheThings() T-Shirt (Red)');
-        await validateProductName(page, 5, 'Sauce Labs Backpack');
+        await productPage.clickSortDropdown();
+        await productPage.selectSortDropdownOption('za');
+        await productPage.validateProductName( 0, 'Test.allTheThings() T-Shirt (Red)');
+        await productPage.validateProductName(5, 'Sauce Labs Backpack');
 
         // sort price (low to high)
-        await sortProducts.click();
-        await sortProducts.selectOption({ value: 'lohi' });
-        await validateProductName(page, 0, 'Sauce Labs Onesie');
-        await validateProductName(page, 5, 'Sauce Labs Fleece Jacket');
+        await productPage.clickSortDropdown();
+        await productPage.selectSortDropdownOption('lohi');
+        await productPage.validateProductName(0, 'Sauce Labs Onesie');
+        await productPage.validateProductName(5, 'Sauce Labs Fleece Jacket');
 
         // sort price (high to low)
-        await sortProducts.click();
-        await sortProducts.selectOption({ value: 'hilo'});
-        await validateProductName(page, 0, 'Sauce Labs Fleece Jacket');
-        await validateProductName(page, 5, 'Sauce Labs Onesie');
+        await productPage.clickSortDropdown();
+        await productPage.selectSortDropdownOption('hilo');
+        await productPage.validateProductName(0, 'Sauce Labs Fleece Jacket');
+        await productPage.validateProductName(5, 'Sauce Labs Onesie');
     });
 });
 
 test.describe('Shopping Cart Page', () => {
-    test('should display products in cart', async ({ page }) => {
-        await loginStandardUser(page);
+    test('should add and remove products in cart', async ({ loginPage, productPage, shoppingCartPage }) => {
+        await loginPage.loginStandardUser();
 
         // add products to cart
-        await addProductToCart(page, 0);
-        await addProductToCart(page, 1);
-        await addProductToCart(page, 2);
+        await productPage.addProductToCart(0)
+        await productPage.addProductToCart(1);
+        await productPage.addProductToCart(2);
 
         // navigate to cart page
-        await page.getByTestId('shopping-cart-link').click();
-        await expect(page.getByTestId('title')).toHaveText('Your Cart');
-        await expect(page.getByTestId('shopping-cart-badge')).toHaveText('3');
+        await productPage.shoppingCartLink.click();
+        await expect(shoppingCartPage.title).toHaveText('Your Cart');
+        await expect(shoppingCartPage.shoppingCartBadge).toHaveText('3');
 
         // assert products displayed in cart
-        await validateProductName(page, 0, 'Sauce Labs Backpack');
-        await validateProductName(page, 1, 'Sauce Labs Bike Light');
-        await validateProductName(page, 2, 'Sauce Labs Bolt T-Shirt');
+        await shoppingCartPage.validateProductName(0, 'Sauce Labs Backpack');
+        await shoppingCartPage.validateProductName(1, 'Sauce Labs Bike Light');
+        await shoppingCartPage.validateProductName(2, 'Sauce Labs Bolt T-Shirt');
 
         // remove products from cart
-        await clickRemoveButton(page);
-        await clickRemoveButton(page);
-        await clickRemoveButton(page);
+        await shoppingCartPage.clickRemoveButton();
+        await shoppingCartPage.clickRemoveButton();
+        await shoppingCartPage.clickRemoveButton();
 
         // assert cart is empty
-        await expect(page.getByTestId('inventory-item')).toHaveCount(0);
+        await expect(shoppingCartPage.inventoryItem).toHaveCount(0);
     });
 });
 
 test.describe('Checkout Workflow', () => {
-    test('should complete checkout', async ({ page }) => {
-        await loginStandardUser(page);
+    test('should complete checkout', async ({ loginPage, productPage, shoppingCartPage,checkoutPage, shippingOverviewPage }) => {
+        await loginPage.loginStandardUser();
 
         // add products to cart
-        await addProductToCart(page, 0);
-        await addProductToCart(page, 1);
-        await expect(page.getByTestId('shopping-cart-badge')).toHaveText('2');
+        await productPage.addProductToCart(0);
+        await productPage.addProductToCart(1);
+        await expect(productPage.shoppingCartBadge).toHaveText('2');
         
         // navigate to checkout page
-        await page.getByTestId('shopping-cart-link').click();
-        await page.getByTestId('checkout').click();
-        await expect(page.getByTestId('title')).toContainText('Information');
+        await productPage.shoppingCartLink.click();
+        await shoppingCartPage.clickCheckoutButton();
+        await expect(checkoutPage.title).toContainText('Information');
 
         // add user information for checkout
-        await page.getByTestId('firstName').fill('adc');
-        await page.getByTestId('lastName').fill('def');
-        await page.getByTestId('postalCode').fill('12345');
-        await page.getByTestId('continue').click();
-        await expect(page.getByTestId('title')).toContainText('Overview');
+        await checkoutPage.enterFirstName('adc');
+        await checkoutPage.enterLastName('def');
+        await checkoutPage.enterPostalCode('12345');
+        await checkoutPage.clickContinueButton();
+        await expect(checkoutPage.title).toContainText('Overview');
 
-        // assert checkout total has value
-        await expect(page.getByTestId('total-label')).not.toBeEmpty();
+        // assert shipping overview page total has value
+        await expect(shippingOverviewPage.cartTotal).not.toBeEmpty();
 
         // navigate to complete checkout
-        await page.getByTestId('finish').click();
-        await expect(page.getByTestId('title')).toContainText('Complete');
+        await shippingOverviewPage.clickFinishButton()
+        await expect(shippingOverviewPage.title).toContainText('Complete');
 
         // navigate back to products from complete page
-        await page.getByTestId('back-to-products').click();
-        await expect(page.getByTestId('title')).toHaveText('Products');
+        await shippingOverviewPage.clickBackToProductsButton();
+        await expect(productPage.title).toHaveText('Products');
     });
 });
-
-// log in helper function
-async function loginStandardUser(page: Page) {
-    await page.getByTestId('username').fill('standard_user');
-    await page.getByTestId('password').fill('secret_sauce');
-    await page.getByTestId('login-button').click();
-}
-// invalid log in helper function
-async function loginInvalidUser(page: Page) {
-    await page.getByTestId('username').fill('invalid_user');
-    await page.getByTestId('password').fill('invalid');
-    await page.getByTestId('login-button').click();
-}
-
-// add product to cart by list position
-async function addProductToCart(page: Page, listPosition: number) {
-    await page.getByTestId('inventory-item').nth(listPosition).getByText('Add to cart').click();
-}
-
-// remove product from cart by list position
-async function removeProductFromCart(page: Page, listPosition: number) {
-    await page.getByTestId('inventory-item').nth(listPosition).getByText('Remove').click();
-}
-
-// remove product from cart page
-async function clickRemoveButton(page: Page) {
-    const removeButton = page.locator('button', { hasText: 'Remove' }).first();
-    await removeButton.click();
-  }
-
-// helper to assert product
-async function validateProductName(page: Page, listPosition: number, productName: string) {
-    await expect(page.getByTestId('inventory-item').nth(listPosition)).toContainText(productName);
-}
